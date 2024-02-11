26a19bade4480835ba360dadca18c326da1be382
xuewenya <s10262410@connect.np.edu.sg>
2024-02-12 02:31:05 +0800

    update DevWeb design

---
+++ js/script.js
@@ -0,0 +1,119 @@
+/* index page typing ainimate */
+const typingTextSpan = document.querySelector(".typing-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["Xue Wen Ya.", "a Web Designer.", "a Web Developer."];
+const typingDelay = 150;
+const erasingDelay = 100;
+const newTextDelay = 1000;
+
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if (!cursorSpan.classList.contains("typing"))
+      cursorSpan.classList.add("typing");
+    typingTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    cursorSpan.classList.remove("typing");
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    if (!cursorSpan.classList.contains("typing"))
+      cursorSpan.classList.add("typing");
+    typingTextSpan.textContent = textArray[textArrayIndex].substring(
+      0,
+      charIndex - 1
+    );
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+    setTimeout(type, typingDelay + 100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(type, newTextDelay);
+});
+
+/* Hamburger menu */
+var menu = document.getElementById("menu");
+var hamburger = document.getElementById("hamburger");
+
+hamburger.addEventListener("click", () => {
+  const mVisibility = menu.getAttribute("data-visible");
+
+  if (mVisibility === "false") {
+    menu.setAttribute("data-visible", true);
+  } else if (mVisibility === "true") {
+    menu.setAttribute("data-visible", false);
+  }
+});
+
+/* filter */
+
+const portfolioBtns = document.querySelectorAll(".portfolio-btn");
+const portfolioItems = document.querySelectorAll(".portfolio-item");
+
+let activeBtn = "all";
+
+showMenu(activeBtn);
+
+portfolioBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    resetActiveBtn();
+    showMenu(btn.id);
+    btn.classList.add("active-btn");
+  });
+});
+
+function resetActiveBtn() {
+  portfolioBtns.forEach((btn) => {
+    btn.classList.remove("active-btn");
+  });
+}
+
+function showMenu(newMenuBtn) {
+  activeBtn = newMenuBtn;
+  portfolioItems.forEach((item) => {
+    if (item.classList.contains(activeBtn)) {
+      item.style.display = "grid";
+    } else {
+      item.style.display = "none";
+    }
+  });
+}
+
+
+/*
+let isSunny = false;
+let isCloudy = true;
+let isEveGoing = true;
+let isTuesday = false;
+let isJaniceGoing = false;
+
+//HTML DOM method of storing the button element references to a var.
+var button = document.getElementById("compute");
+
+//using the var button reference to add an event listener.
+button.addEventListener("click", function() {
+
+  if (isEveGoing && isCloudy) {
+    //changing background color of the html element via javascript through html dom method.
+    document.getElementById("Going").style.backgroundColor = "green";
+
+  } else {
+    //changing background color of the html element via javascript through html dom method.
+    document.getElementById("notGoing").style.backgroundColor = "green";
+  }
+});
+*/
